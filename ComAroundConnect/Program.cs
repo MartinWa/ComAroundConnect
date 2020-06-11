20163be4b5be7d1f1ae59bde7f119ff09469861d
Martin Wåger <martin.wager@comaround.com>
2020-06-11 13:46:10 +0200

    Added listing of all tables

--- ComAroundConnect/Program.cs
+++ ComAroundConnect/Program.cs
@@ -5,6 +5,7 @@ namespace ComAroundConnect
 {
     class Program
     {
+
         static void Main()
         {
             const string Server = "";
@@ -12,11 +13,26 @@ namespace ComAroundConnect
             const string Password = "";
             const string Model = "";
 
+            if (string.IsNullOrEmpty(Server) || string.IsNullOrEmpty(UserName) || string.IsNullOrEmpty(Password) || string.IsNullOrEmpty(Model))
+            {
+                Console.WriteLine("You must set server/username/password/model");
+            }
+            else
+            {
+                var connectionString = $"Provider=MSOLAP;Data Source=asazure://westeurope.asazure.windows.net/{Server};Initial Catalog={Model};User ID={UserName};Password={Password};Persist Security Info=True;Impersonation Level=Impersonate";
+                ListVotesPerYear(connectionString);
+                RetrieveCubesAndDimensions(connectionString);
+            }
+            Console.WriteLine("Press any key");
+            Console.ReadLine();
+        }
 
+        private static void ListVotesPerYear(string connectionString)
+        {
             // Code from https://csharp.hotexamples.com/examples/-/AdomdConnection/Open/php-adomdconnection-open-method-examples.html
             using (AdomdConnection mdConn = new AdomdConnection())
             {
-                mdConn.ConnectionString = $"Provider=MSOLAP;Data Source=asazure://westeurope.asazure.windows.net/{Server};Initial Catalog={Model};User ID={UserName};Password={Password};Persist Security Info=True;Impersonation Level=Impersonate";
+                mdConn.ConnectionString = connectionString;
                 mdConn.Open();
                 AdomdCommand mdCommand = mdConn.CreateCommand();
 
@@ -25,10 +41,10 @@ namespace ComAroundConnect
                         NON EMPTY[DimDate].[Year].members ON 0,
                         [Measures].[Votes] ON 1
                     FROM[Model]"; // << MDX Query 
-                CellSet cs = mdCommand.ExecuteCellSet(); 
+                CellSet cs = mdCommand.ExecuteCellSet();
                 if (cs.Axes.Count != 2) return;
                 TupleCollection tuplesOnColumns = cs.Axes[0].Set.Tuples;
-                TupleCollection tuplesOnRows = cs.Axes[1].Set.Tuples; 
+                TupleCollection tuplesOnRows = cs.Axes[1].Set.Tuples;
                 Console.Write("{0,-12}", "Item");
                 for (int col = 0; col < tuplesOnColumns.Count; col++)
                 {
@@ -42,7 +58,39 @@ namespace ComAroundConnect
                     { Console.Write("{0,-12}", cs.Cells[col, row].Value); }
                     Console.WriteLine();
                 }
-                Console.ReadLine();
+                mdConn.Close();
+            }
+        }
+
+        private static void RetrieveCubesAndDimensions(string connectionString)
+        {
+            // From https://docs.microsoft.com/en-us/analysis-services/adomd/multidimensional-models-adomd-net-client/retrieving-metadata-working-with-adomd-net-object-model?view=asallproducts-allversions
+
+            //Connect to the local server
+            using (AdomdConnection conn = new AdomdConnection(connectionString))
+            {
+                conn.Open();
+
+                //Loop through every cube
+                foreach (CubeDef cube in conn.Cubes)
+                {
+                    //Skip hidden cubes.
+                    if (cube.Name.StartsWith("$"))
+                        continue;
+
+                    //Write the cube name
+                    Console.WriteLine(cube.Name);
+
+                    //Write out all dimensions, indented by a tab.
+                    foreach (Dimension dim in cube.Dimensions)
+                    {
+                        Console.WriteLine("\t");
+                        Console.WriteLine(dim.Name);
+                    }
+                }
+
+                //Close the connection
+                conn.Close();
             }
         }
     }
